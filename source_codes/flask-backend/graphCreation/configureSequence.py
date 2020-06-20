92a2e5ba61ce9cc989458b923d4239e565018c00
olexstet <olexstet1998@gmail.com>
2020-06-20 23:39:54 +0200

    Update configureSequence.py
    Add comments

--- source_codes/flask-backend/graphCreation/configureSequence.py
+++ source_codes/flask-backend/graphCreation/configureSequence.py
@@ -1,16 +1,17 @@
+# if it exists gaps of levels in sequence, add the missing levels with All property  
 def configureSequence(sequence):
     configuredSequence = [sequence[0]]
-    for i in range(1,len(sequence)):
+    for i in range(1,len(sequence)): # if gaps between levels 
         if sequence[i][0]-1 != sequence[i-1][0]:
             step = sequence[i][0] - sequence[i-1][0]
             sequenceToAdd = []
-            startValue = sequence[i-1][0]
-            endValue = sequence[i][0]
+            startValue = sequence[i-1][0] # previous 
+            endValue = sequence[i][0]# current 
             for j in range(startValue+1,endValue):
-                sequenceToAdd.append([j,"All"])
+                sequenceToAdd.append([j,"All"]) # add missing level 
             
-            configuredSequence += sequenceToAdd + [sequence[i]]
+            configuredSequence += sequenceToAdd + [sequence[i]] # add to existent sequence 
         else:
-            configuredSequence += [sequence[i]]
+            configuredSequence += [sequence[i]] # if no gaps in level 
         
     return configuredSequence 
