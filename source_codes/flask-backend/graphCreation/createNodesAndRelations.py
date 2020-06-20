0485d27f78a2e9447acd34ac63c0a801d2775f76
olexstet <olexstet1998@gmail.com>
2020-06-20 23:36:09 +0200

    Update createNodesAndRelations.py
    Add comments and remove unusefull function addToFile

--- source_codes/flask-backend/graphCreation/createNodesAndRelations.py
+++ source_codes/flask-backend/graphCreation/createNodesAndRelations.py
@@ -1,7 +1,8 @@
 from graphCreation.convertSynsetToString import *
 from nltk.corpus import wordnet as wn
 
-def generateLabel(labelSequence, separation):
+# create a custom property for node (label)
+def generateLabel(labelSequence, separation): # string sequence , separation = ":",",", "anything"
     result = ""
     for i in range(len(labelSequence)):
         element = labelSequence[i]
@@ -11,30 +12,22 @@ def generateLabel(labelSequence, separation):
             result += element 
     return result
 
-def addToFile(string):
-    f = open("graphCreation/cypherScript.txt", "r")
-    saveD = f.read()
-    f.close()
-    f = open("graphCreation/cypherScript.txt", "w")
-    f.write(saveD)
-    f.write(string+"\n")
-    f.close()
+# Create a node for a term
+def createNode(S,graph): # synset, graph 
+    result = convertHypernymsToString([S]) # convert from synset to string (name of the synset)
+    defType = {"n": "Name", "v": "Verbe", "a": "Adjective", "r": "Adverbe"} # possible types of words 
 
-def createNode(S,graph):
-    result = convertHypernymsToString([S])
-    defType = {"n": "Name", "v": "Verbe", "a": "Adjective", "r": "Adverbe"}
+    w = result[0] # fetch term 
 
-    w = result[0]
-
-    typeW = S.pos()
-    typeW = defType[typeW]
+    typeW = S.pos() # fetch type of synset 
+    typeW = defType[typeW] # translate type into prefered type 
 
     label = generateLabel([w,typeW], "::")
 
     definition = wn.synsets(w)[0].definition()
     definition = definition.replace('"','')
 
-    query = "Match (t:Term) return t"
+    query = "Match (t:Term) return t" # neo4j query for fetching all nodes 
 
     countNodes = 0
     nodes = graph.run(query)
@@ -46,47 +39,44 @@ def createNode(S,graph):
     if w in names:
         countNodes += 1
         
-    if countNodes == 0:
+    if countNodes == 0: # if no similar nodes exist, create new node 
         query = 'MERGE (t:Term {name: '+'"'+w+'", definition: "'+ definition + '" , type: "'+typeW+'" , label: "'+label+'" })'
-        string = str('MERGE (t:Term {name: '+'"'+w+'", definition: "'+ definition + '" , type: "'+typeW+'" , label: "'+label+'" })')
-        addToFile(string)
         graph.run(query)
 
+#create relations between two synsets 
 def createRelation(S1,S2,graph):
     result = convertHypernymsToString([S1,S2])
-    w1 = result[0]
+    w1 = result[0] # fetch term name 
     w2 = result[1]
     #print(w1,w2)
 
     existsW1 = False 
     existsW2 = False 
 
-    query = '''Match (t:Term) Where t.name = "''' + w1 + '''" Return t'''
+    query = '''Match (t:Term) Where t.name = "''' + w1 + '''" Return t''' # find first term 
     nodes = graph.run(query)
 
     for node in nodes: 
         existsW1 = True
 
-    query = '''Match (t:Term) Where t.name = "''' + w2 + '''" Return t'''
+    query = '''Match (t:Term) Where t.name = "''' + w2 + '''" Return t''' # find second term 
     nodes = graph.run(query)
 
     for node in nodes:
         existsW2 = True
 
-    if existsW1 == True and existsW2 == True:
-        query = '''MATCH (t1:Term)-[]-(t2:Term) WHERE t1.name = "''' + w1 + '''" and t2.name = "''' + w2 +'''" Return t2'''
+    if existsW1 == True and existsW2 == True: # if both exist 
+        query = '''MATCH (t1:Term)-[]-(t2:Term) WHERE t1.name = "''' + w1 + '''" and t2.name = "''' + w2 +'''" Return t2''' # look if a relation exists already a 
         nodes = graph.run(query)
         countNodes = 0
                             
         for node in nodes:
             countNodes += 1
                             
-        if countNodes == 0:
+        if countNodes == 0: # if no relations, create one 
             query = '''MATCH (t1:Term),(t2:Term) WHERE t1.name = "''' + w1 + '''" and t2.name = "''' + w2 +'''" 
-            MERGE (t1)-[:ISA]->(t2)''' # add property of order 
+            MERGE (t1)-[:ISA]->(t2)'''  
             graph.run(query)   
-            string = '''MATCH (t1:Term),(t2:Term) WHERE t1.name = "''' + w1 + '''" and t2.name = "''' + w2 +'''" 
-            \n MERGE (t1)-[:ISA]->(t2)'''
-            addToFile(string)         
+                   
         return True
     return False  
