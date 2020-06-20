4e25c9fbb80378d9cb74959acebc77a87ffa4725
olexstet <olexstet1998@gmail.com>
2020-06-20 23:57:41 +0200

    Update generateHypernyms.py
    Add comments for understanding

--- source_codes/flask-backend/graphCreation/hypernymsCreation/generateHypernyms.py
+++ source_codes/flask-backend/graphCreation/hypernymsCreation/generateHypernyms.py
@@ -10,27 +10,27 @@ from graphCreation.createNodesAndRelations import *
 from graphCreation.convertSynsetToString import *
 from graphCreation.pathsCreation import *
 
-def removeNonExistedNodes(allPaths, graph):
+def removeNonExistedNodes(allPaths, graph): # remove nodes which are not present in the graph 
     results = []
     for array in allPaths: 
         result = []
         for word in array: 
-            w = convertHypernymsToString([word])[0]
-            query = '''Match (t:Term) Where t.name = "''' + w + '''" Return t'''
+            w = convertHypernymsToString([word])[0] # fetch name of synset 
+            query = '''Match (t:Term) Where t.name = "''' + w + '''" Return t''' # check if term exists in graph 
             nodes = graph.run(query)
             
             existsW = False 
 
-            for node in nodes: 
+            for node in nodes: # if node exists, set to true 
                 existsW = True
 
             if existsW: 
-                result.append(word)
+                result.append(word) # if exists add to array for saving the node 
             
         results.append(result)
-    return results 
+    return results # return array of array of nodes present in the graph, also can be see as all paths of existent nodes 
 
-def checkOtherMeanings(word):
+def checkOtherMeanings(word): # not used, but can be used for future development (check if the term has several meaning in wordNet )
     words = []
     synonyms = wn.synsets(word)
     for s in synonyms: 
@@ -40,43 +40,44 @@ def checkOtherMeanings(word):
 
 def findHypernyms(word,dist,numberHyp,previousHypernyms,paths):
     result = []
-    for synset in previousHypernyms: 
-        hypernyms = synset.hypernyms()
+    for synset in previousHypernyms: # look on previous hypernyms founded 
+        hypernyms = synset.hypernyms() # find all hypernyms based on synset 
         #print(synset, hypernyms)
-        if type(numberHyp) == int: 
+        if type(numberHyp) == int: # if the number of hypernyms needed is not 'All' and is integer 
             length = len(hypernyms)
-            if length > numberHyp: 
-                result += hypernyms[:numberHyp]
-                paths[(dist,synset)] = hypernyms[:numberHyp]
-            else: 
+            if length > numberHyp: # fetch only the number of needed hypernyms, if it exists more than we need 
+                result += hypernyms[:numberHyp] # add to new founded hypernyms 
+                paths[(dist,synset)] = hypernyms[:numberHyp] # add to the path 
+            else: # if exactly the number of hypernyms needed or lower, take all possible hypernyms for synset 
                 result += hypernyms 
                 paths[(dist,synset)] = hypernyms
-        else:
-            result += hypernyms
+        else: # if 'All' hypernyms needed 
+            result += hypernyms 
             paths[(dist,synset)] = hypernyms
 
-    previousHypernyms = result
+    previousHypernyms = result # store all new founded hypernyms 
 
     previousHypernyms = list(dict.fromkeys(previousHypernyms))
     return previousHypernyms,paths
 
 
-def generateHypernyms(sequenceHyp, word, graph):
-    previousHypernyms = [wn.synsets(word)[0]]
-    sequenceHyp = configureSequence(sequenceHyp)
+def generateHypernyms(sequenceHyp, word, graph): # allow to create hypernyms in the graph for a term 
+    previousHypernyms = [wn.synsets(word)[0]] # fecth initial term 
+    sequenceHyp = configureSequence(sequenceHyp) # fill the gaps 
     result = {}
     result[(0,0)] = previousHypernyms
     paths = {}
     dist = 0
     numberHyp = 0
-    for seq in sequenceHyp:
-        dist = seq[0]
-        numberHyp = seq[1]
-        previousHypernyms,paths = findHypernyms(word,dist,numberHyp,previousHypernyms,paths)
+    for seq in sequenceHyp:# go though the sequence of needed hypernyms (level, number hypernyms)
+        dist = seq[0] # level 
+        numberHyp = seq[1] # number needed 
+        previousHypernyms,paths = findHypernyms(word,dist,numberHyp,previousHypernyms,paths) # find for a level the hypernyms 
           
         if (type(dist) and type(numberHyp)) == int:
-            result[(seq[0],seq[1])] = previousHypernyms
+            result[(seq[0],seq[1])] = previousHypernyms # founded hypernyms are previous hypernyms for next iteration 
 
+    # Create nodes
     createdNodes = []
     for seq in result:
         synsets = result[seq]
@@ -84,22 +85,20 @@ def generateHypernyms(sequenceHyp, word, graph):
             createNode(synset,graph)
             createdNodes.append(synset)
         
-    _,paths = findHypernyms(word,dist+1,numberHyp,previousHypernyms,paths)
+    _,paths = findHypernyms(word,dist+1,numberHyp,previousHypernyms,paths) # add next level hypernyms 
 
-    res = formPaths(paths,wn.synsets(word)[0])
-    #print(res)
+    res = formPaths(paths,wn.synsets(word)[0]) # all the possible paths from inital term to all hypernyms 
     
-    res = removeNonExistedNodes(res, graph)
-    #print("\nRelations hypernyms")
+    res = removeNonExistedNodes(res, graph) # remove nodes which don't exist in the graph 
+    
+    # Create Relations 
     createdRel = []
     for r in res: 
         for i in range(1,len(r)):
             if [r[i],r[i-1]] not in createdRel:
                 createRelation(r[i-1],r[i],graph)
                 createdRel.append([r[i],r[i-1]])
-                #print(r[i-1],r[i])
         
-    #print("--------------------------------------------------------------")
     return result
 
 #generateHypernyms([[1,5],[5,3]], "apple", connect())
