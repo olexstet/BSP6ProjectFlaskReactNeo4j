e60474e24c774735e9dcfa1edb1c0520beb40440
olexstet <olexstet1998@gmail.com>
2020-06-21 01:14:39 +0200

    Update Q3FetchData.py
    Add some comments

--- source_codes/flask-backend/generators/Q3FetchData.py
+++ source_codes/flask-backend/generators/Q3FetchData.py
@@ -6,25 +6,25 @@ import requests
 import urllib.request
 from generators.Q2FetchData import * 
 
-def retrieveSubWords(word, levelDown, numberWords, graph):
+def retrieveSubWords(word, levelDown, numberWords, graph): # retieve hyponyms at a level 
     arrayWords = []
 
-    query = ''' Match (t1:Term)<-[:ISA*'''+str(levelDown)+''']-(t2:Term) Where t1.name ="'''+word+'''" Return t2'''
+    query = ''' Match (t1:Term)<-[:ISA*1..'''+str(levelDown)+''']-(t2:Term) Where t1.name ="'''+word+'''" Return t2''' # fetch hyponyms for a level 
     nodes = graph.run(query)
 
     for node in nodes:
         arrayWords.append(node[0]['name'])
 
     random.shuffle(arrayWords)
-    arrayWords = arrayWords[:numberWords]
+    arrayWords = arrayWords[:numberWords] # take n words of hyponyms 
     return arrayWords
 
 
 def createQuestion3Words(word, levelDown, numberWordsTotal):
     typeW = typeWord(word, connect())
     numberSubWords = random.randint(1,numberWordsTotal)
-    wordsCategory = retrieveSubWords(word,levelDown,numberSubWords, connect())
-    randomWords = chooseRandomWords(fetchAllWords(),numberWordsTotal-len(wordsCategory), typeW)
+    wordsCategory = retrieveSubWords(word,levelDown,numberSubWords, connect()) # correct words 
+    randomWords = chooseRandomWords(fetchAllWords(),numberWordsTotal-len(wordsCategory), typeW) # random words 
     dictWords = {}
     dictWords[word] = wordsCategory
     dictWords['random'] = randomWords
