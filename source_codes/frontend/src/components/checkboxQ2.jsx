279e057762186941b6f3176bd42303492d9b4315
olexstet <olexstet1998@gmail.com>
2020-06-21 02:09:44 +0200

    Update checkboxQ2.jsx
    Add comment

--- source_codes/frontend/src/components/checkboxQ2.jsx
+++ source_codes/frontend/src/components/checkboxQ2.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Checkbox } from 'semantic-ui-react';
 
-export class checkboxQ2 extends Component {
+export class checkboxQ2 extends Component { /* Checkbox for word of question 2*/
     render() {
         return (
             <div>
